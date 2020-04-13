0d0d3518e7a9732a57ddb73e8e818b174f0bf3ba
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-13 05:43:26 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,17 @@
 # #second_challenge
 #   fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs
 
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      patriarch: {},            #your key/value pairs here!
-      matriarch: {},
-      hero: {},
-      hero_friends: []
-    },
-    :capulet => {
-      patriarch: {},            #your key/value pairs here!
-      matriarch: {},
-      heroine: {},
-      heroine_friends: []
-    }
-  }
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
 }
